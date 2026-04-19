6900d033fa566d396c9cda0c6ba095334b31dedc
maialfakharany6m-wq <mai.alfakharany6m@gmail.com>
2026-04-19 22:33:11 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
-
-window.onerror = function (msg, url, line, col, error) {
-  console.error("GLOBAL ERROR:", error);
-};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
